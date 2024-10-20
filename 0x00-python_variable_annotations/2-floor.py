2e03de1003a79f1acbfc21f8fc624721ae0aaf00
Davies Magare <davieskamanda@gmail.com>
2024-10-20 05:42:21 +0000

    Task 2 first attempt

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import math
+
+"""A function that returns the floor of a number"""
+
+
+def floor(n: float) -> int:
+    """
+    Args:
+        n: A float
+
+    Returns:
+        The floor of n
+
+    """
+    return math.floor(n)
