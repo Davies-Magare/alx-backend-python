f01404f08d1fa2654809bf54ab2488e77edd6c2c
Davies Magare <davieskamanda@gmail.com>
2024-10-20 08:01:12 +0000

    Task 4 first attempt

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module demonstrates basic annotation in python using different
+variable types and their specified values
+"""
+
+
+# integer variable
+a: int = 1
+
+
+# float variable
+pi: float = 3.14
+
+
+# Boolean variable
+i_understand_annotations: bool = True
+
+
+
+# String variable
+
+school: str = "Holberton"
