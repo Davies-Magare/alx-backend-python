bd85668093a7f222f8b16e87e5ce65b8f6ffaf00
Davies Magare <davieskamanda@gmail.com>
2024-10-20 06:38:46 +0000

    Add documentation and tests to task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python3
-"""Basic annotations with a function that concantenates two strings"""
+"""This module demonstrates basic type annotations in Python
+with a function that concantenates two strings
+
+Functions:
+    concat(str1: str, str2: str) -> str:
+        Concantenates two strings
+"""
 
 
 def concat(str1: str, str2: str) -> str:
     """
+    Concantenates two strings
     Args:
-        str1: The first parameter
-        str2: The second parameter
+        str1(string) : The first string to be concantenated to the 
+                       second string
+        str2(string) : The second string to be concantenated to the
+                        first string
 
     Returns:
-        The result of concantenating str1 and str2
+        str: The result of concantenating str1 and str2
+    Example:
+        >>> concat("Davies", " Magare")
+        'Davies Magare'
+        >>> concat('', '')
+        ''
+        >>> concat("egg", "shell") == "{}{}".format("egg", "shell")
+        True
+        >>> concat(4, "dog")
+        Traceback (most recent call last):
+            ...
+        TypeError: unsupported operand type(s) for +: 'int' and 'str'
+        >>> concat("dog", 4)
+        Traceback (most recent call last):
+            ...
+        TypeError: can only concatenate str (not "int") to str
 
     """
     return str1 + str2
