4fff608d0551186f8e2ecb232ec89f53a44d5770
Davies Magare <davieskamanda@gmail.com>
2024-10-20 05:29:25 +0000

    Task 1 first attempt

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic annotations with a function that concantenates two strings"""
+
+
+def add(str1: str, str2: str) -> str:
+    """
+    Args:
+        str1: The first parameter
+        str2: The second parameter
+
+    Returns:
+        The result of concantenating str1 and str2
+
+    """
+    return str1 + str2
