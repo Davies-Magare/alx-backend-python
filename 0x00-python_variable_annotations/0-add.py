c6176ab4e9a77fd41dba1bf3c323aa17ef9ddff4
Davies Magare <davieskamanda@gmail.com>
2024-10-20 05:00:34 +0000

    Task 0 first attempt

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+"""Basic annotations with a function that adds two integers"""
+
+def add(a: float, b: float) -> float:
+    """Add two floats
+    Args:
+        a: The first parameter
+        b: The second parameter
+
+    Returns:
+        The sum of a and b
+
+    """
+    return a + b
