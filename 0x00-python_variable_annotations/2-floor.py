3824101e1850f5a003f9fb2c8039323ee32cc93e
Davies Magare <davieskamanda@gmail.com>
2024-10-20 07:19:01 +0000

    Add proper documentation to function

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,16 +1,49 @@
 #!/usr/bin/env python3
 import math
 
-"""A function that returns the floor of a number"""
+"""This module demonstrates basic type annotations in Python
+with a function that converts a float to its floor integer
+
+Imports:
+    math: Used for the floor function to compute the largest
+        integer less or equal to the given float.
+
+Functions:
+    floor(n: float) -> int:
+        Converts a float to its floor.
+"""
 
 
 def floor(n: float) -> int:
     """
+    Converts a float to its floor integer
+
     Args:
-        n: The frist parameter: A float
+        n (float): The only parameter: A floating-point number
+            to convert to its floor integer.
 
     Returns:
-        The floor of n
+        int: The integer floor of n.
+
+    Example:
+        >>> floor(3.5)
+        3
+        >>> floor(-4)
+        Traceback (most recent call last):
+                ...
+        TypeError: n must be a floating-point number
+
+    Raises:
+        TypeError: If n is not a floating-point number.
 
     """
+    if not isinstance(n, float):
+        raise TypeError('n must be a floating-point number')
+
     return math.floor(n)
+
+
+# If running this module directly, execute doctests.
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
