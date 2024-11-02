eafcfd58b78027af2d9a1355b087c52d28e28a99
Davies Magare <davieskamanda@gmail.com>
2024-11-02 11:47:17 +0000

    Complete first attempt for task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+This module demonstrates how to run multiple
+coroutines at the same time with async
+"""
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def qsort(arr):
+    """Sort the list with qsort"""
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    left = []
+    right = []
+    for i in arr[1:]:
+        if i < pivot:
+            left.append(i)
+        else:
+            right.append(i)
+    return qsort(left) + [pivot] + qsort(right)
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Run multiple coroutines."""
+
+    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    results = await asyncio.gather(*tasks)
+    return qsort(results)
