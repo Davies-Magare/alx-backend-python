dee9d8b1c7abcafe14a4ebfcd2609c5aa7e16851
Davies Magare <davieskamanda@gmail.com>
2024-11-05 02:48:02 +0000

    Fix type annotation for task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,10 +7,10 @@ number between 0 and 10
 
 import asyncio
 import random
-import typing
+from typing import Generator
 
 
-async def async_generator() -> typing.Generator[float, NoneType, NoneType]:
+async def async_generator() -> typing.Generator[float, None, None]:
     """Generate a random number between 0 to 10"""
 
     for i in range(0, 10):
