9505195dee4f5ad04cf76ef087f681658f2daa85
Davies Magare <davieskamanda@gmail.com>
2024-11-10 09:17:35 +0000

    Complete first attempt for task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,6 +12,7 @@ from unittest.mock import patch, Mock
 
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -51,6 +52,29 @@ class TestGetJson(unittest.TestCase):
         response = get_json(url)
         mock_request.assert_called_once_with(url)
         self.assertEqual(get_json(url), ret_dict)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test memoize  method"""
+
+    def test_memoize(self):
+        """Test memoize"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        instance = TestClass()
+        instance.a_method = Mock()
+        instance.a_method.return_value = 42
+        result = instance.a_property
+        result2 = instance.a_property
+        for result in [result, result2]:
+            self.assertEqual(42, result)
+        instance.a_method.assert_called_once()
 
 
 if __name__ == '__main__':
