b9a3af447e9d027def21a05abd7c8735d9f40f41
Davies Magare <davieskamanda@gmail.com>
2024-11-11 10:06:30 +0000

    Correct syntax for task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,37 +7,33 @@ using parameterized and Mocks
 
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
-from client import GithubOrgClient  # Assuming this is the path
+from unittest.mock import patch, Mock
+
+access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
+GitClient = __import__('client').GithubOrgClient
+
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test GithubOrgClient class"""
 
     @parameterized.expand([
-        ('google', {"repos_url": "https://api.github.com/orgs/google/repos"}),
-        ('abc', {"repos_url": "https://api.github.com/orgs/abc/repos"})
+        ('google', {"git": "myorg"}),
+        ('abc', {"git": "myorg"})
     ])
-    @patch('client.get_json')  # Patch `get_json` in the `client` module
-    def test_org(self, org, result, mock_get_json):
-        """Test that GithubOrgClient.org returns the correct value"""
+    @patch('utils.requests.get')
+    def test_org(self, org, result, mock_request):
+        """Test the org method"""
 
-        # Set the return value for `get_json` mock
-        mock_get_json.return_value = result
-
-        # Create an instance of GithubOrgClient
-        instance = GithubOrgClient(org)
-
-        # Call the `org` method (this will call `get_json` internally)
-        org_data = instance.org
-
-        # Check if `get_json` was called with the correct URL
-        expected_url = f"https://api.github.com/orgs/{org}"
-        mock_get_json.assert_called_once_with(expected_url)
-
-        # Check that the result from `org` matches the mock return value
-        self.assertEqual(org_data, result)
+        ORG_URL = "https://api.github.com/orgs/{}"
+        response = mock_request.return_value
+        response.json.return_value = result
+        mock_request.return_value = response
+        instance = GitClient(org)
+        self.assertEqual(instance.org, result)
+        mock_request.assert_called_once_with(ORG_URL.format(org))
 
 
 if __name__ == '__main__':
     unittest.main()
-
