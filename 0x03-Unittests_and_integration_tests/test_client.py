7056e336788ee5ae655a78afd57ac4a27005a112
Davies Magare <davieskamanda@gmail.com>
2024-11-11 10:23:31 +0000

    Shift mock to right function

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,7 +8,7 @@ using parameterized and Mocks
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-
+from typing import Dict
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
 memoize = __import__('utils').memoize
@@ -22,14 +22,12 @@ class TestGithubOrgClient(unittest.TestCase):
         ('google', {"git": "myorg"}),
         ('abc', {"git": "myorg"})
     ])
-    @patch('utils.requests.get')
-    def test_org(self, org, result, mock_request):
+    @patch('client.get_json')
+    def test_org(self, org: str, result, mock_request):
         """Test the org method"""
 
         ORG_URL = "https://api.github.com/orgs/{}"
-        response = mock_request.return_value
-        response.json.return_value = result
-        mock_request.return_value = response
+        mock_request.return_value = result
         instance = GitClient(org)
         self.assertEqual(instance.org, result)
         mock_request.assert_called_once_with(ORG_URL.format(org))
