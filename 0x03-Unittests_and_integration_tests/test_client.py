0346421b36ee4808a0dbd751456ca34009739460
Davies Magare <davieskamanda@gmail.com>
2024-11-12 09:45:46 +0000

    Fix mock for property for task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -45,14 +45,15 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_public_repos(self, mock_request):
         """Test public_repos method"""
-        with patch('client.GithubOrgClient._public_repos_url')as mock_url:
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock)as mock_url:
             mock_request.return_value = [
                 {"name": "dart"}, {"name": "cpp-netlib"}]
             mock_url.return_value = "https://api.github.com/orgs/google/repos"
             instance = GitClient("google")
             self.assertEqual(instance.public_repos(), ["dart", "cpp-netlib"])
             mock_request.assert_called_once()
-            # mock_url.assert_called_once()
+            mock_url.assert_called_once()
 
 
 if __name__ == '__main__':
